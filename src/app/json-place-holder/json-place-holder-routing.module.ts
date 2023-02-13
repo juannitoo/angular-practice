0c5d1a0660f8e63ed0c1fe4f00eeb5b0dbbfa958
Jean Balangue <jeanbalangue@hotmail.fr>
2023-02-13 15:47:19 +0100

    renommage jsonplaceholder users -> users-list

--- src/app/json-place-holder/json-place-holder-routing.module.ts
+++ src/app/json-place-holder/json-place-holder-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JsonLandingComponent } from './components/json-landing/json-landing.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
 const routes: Routes = [
-    { path : '', component: JsonLandingComponent}
+    { path : '', component: JsonLandingComponent },
+    { path: 'users', component: UsersListComponent }
 ];
 
 @NgModule({
--- src/app/json-place-holder/json-place-holder.module.ts
+++ src/app/json-place-holder/json-place-holder.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JsonLandingComponent } from './components/json-landing/json-landing.component';
-import { UsersComponent } from './components/users/users.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 import { JsonPlaceHoldeRoutingModule } from './json-place-holder-routing.module';
 
 
@@ -9,7 +9,7 @@ import { JsonPlaceHoldeRoutingModule } from './json-place-holder-routing.module'
 @NgModule({
   declarations: [
     JsonLandingComponent,
-    UsersComponent
+    UsersListComponent
   ],
   imports: [
     CommonModule,
