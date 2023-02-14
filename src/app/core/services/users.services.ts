33b90515521c3eaf7ebc808bfd28637ebf7a86cd
Jean Balangue <jeanbalangue@hotmail.fr>
2023-02-14 17:46:37 +0100

    debut de crud ds userService et userComponent, manque les forms a faire

--- src/app/core/services/users.services.ts
+++ src/app/core/services/users.services.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap, map } from 'rxjs';
 import { User } from 'src/app/core/models/user.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+    // Si le back end faisait vraiment son taf, il me semble que ca fonctionnerait
+    // faut que je me renseigne sur la gestion des erreurs
 
-    constructor( private http : HttpClient ) { }
+    constructor( private http : HttpClient,
+                 private router : Router ) { }
 
     getUsers(): Observable<User[]> {
-        return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users')
+        return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
     }
 
     getUser(userId: number): Observable<User> {
-        return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${userId}`);
     }
     
+    deleteUser(userId: number): Observable<User>{
+        return this.http.delete<User>(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    }
+
+    addUser(formValue: User): Observable<User>{
+        return this.http.post<User>(`https://jsonplaceholder.typicode.com/users`,
+        formValue);
+    }
+
+    updateUser(userId: number, formValue: User ){
+        return this.http.put<User>(`https://jsonplaceholder.typicode.com/users`,
+        formValue);
+    }
     
 }
+
--- src/app/json-place-holder/components/user/user.component.ts
+++ src/app/json-place-holder/components/user/user.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { User } from 'src/app/core/models/user.model';
 import { UsersService } from 'src/app/core/services/users.services';
 import { Router, ActivatedRoute } from '@angular/router';
+import { tap, map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-user',
@@ -14,32 +17,65 @@ export class UserComponent implements OnInit {
   // enfant de la liste de user-list
   @Input() user!: User;
   // lorsque url par ex user/1
-  user$! : Observable<User>
-  buttonText! : string;
+  user$! : Observable<User>;
 
   constructor(private usersService: UsersService,
               private route : ActivatedRoute,
-              private router : Router) { }
+              private router : Router,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.buttonText = "Afficher"
-    const userId = +this.route.snapshot.params['id']
+    const userId = +this.route.snapshot.params['id'];
     if (userId) {
       this.user$ = this.getUser(userId);
-      this.buttonText = "Retour à la liste"
     }
   }
   
+  showUser(userId : number){
+    return this.router.navigateByUrl(`jsonplaceholder/users/${userId}`);
+  }
+
+  backToList(){
+    return this.router.navigateByUrl('jsonplaceholder/users');
+  }
+
+  //CRUD
   getUser(userId:number): Observable<User> {
     return this.usersService.getUser(userId);
   }
 
-  showUser(userId : number){
-    return this.router.navigateByUrl(`jsonplaceholder/users/${userId}`)
+  delUser(userId: number){
+    return this.usersService.deleteUser(userId)
+    .pipe(
+      //au retour de l'observable, je loggue
+      tap(() => console.log("user supprimé !")),
+      // je redirige
+      map( (util) => this.router.navigateByUrl('jsonplaceholder/users'))
+    ).subscribe();
+
+      // ca je garde pour batailler un peu.
+      // map((user) => { 
+      //   const index = this.usersService.getUsers().pipe(
+      //     map( util => [...util]),
+      //     map( (util) => { 
+      //       const index = util.findIndex((u)=> u.id === userId);
+      //       util.splice(index,1);
+      //       return util
+      //     }),
+      //     tap( util =>  console.log('les users modifiés', util)),
+      //     map( (util) => this.router.navigateByUrl('jsonplaceholder/users'))
+      //   ).subscribe();
+      // })
   }
 
-  backToList(){
-    return this.router.navigateByUrl('jsonplaceholder/users')
+  addUser(user: User){
+    console.log(user)
   }
+
+  updateUser(userId:number){
+    // formModule ou reactive formmodule. A voir demain ...
+    console.log(userId)
+  }
+
 
 }
