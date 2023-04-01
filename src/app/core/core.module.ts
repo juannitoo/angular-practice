ea1c6b7d2a2b76d49a0c114d9d12f489bb6c4fda
Jean Balangue <jeanbalangue@hotmail.fr>
2023-04-01 11:56:27 +0200

    ajout component footer + css home

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,12 +6,14 @@ import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '../shared/shared.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -22,6 +24,7 @@ import { SharedModule } from '../shared/shared.module';
   providers: [HttpClient],
   exports: [
     HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
@@ -16,7 +15,6 @@ import { MatFormFieldModule } from "@angular/material/form-field";
     CommonModule
   ],
   exports: [
-    // MatToolbarModule,
     MatInputModule,
     MatCardModule,
     MatListModule,
