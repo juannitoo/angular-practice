a7fc45c92925531232afc24771145b291d1c25f2
Jean Balangue <jeanbalangue@hotmail.fr>
2023-04-01 09:38:18 +0200

    css images et petites retouches

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,7 +1,6 @@
-import { animate, animateChild, query, stagger, state, style, transition, trigger, useAnimation } from '@angular/animations';
+import { animateChild, query, stagger, transition, trigger, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FadeInColorAnimation } from 'src/app/shared/animations/fade-in-color.animation';
-import { SlideAndFadeAnimation } from 'src/app/shared/animations/slide-and-fade.animation';
 
 @Component({
   selector: 'app-header',
@@ -30,34 +29,11 @@ import { SlideAndFadeAnimation } from 'src/app/shared/animations/slide-and-fade.
         )
       ])
     ]),
-    trigger( 'slideLogo', [
-      transition(':enter', [
-        useAnimation(SlideAndFadeAnimation, {
-          params: {
-              timer: '700ms',
-              delay: '0ms',
-              direction: 'X',
-              directionValue: '-200%'
-          }}
-        )
-      ])
-    ]),
-    trigger( 'slideNav', [
-      transition(':enter', [
-        useAnimation(SlideAndFadeAnimation, {
-          params: {
-              timer: '700ms',
-              delay: '0ms',
-              direction: 'X',
-              directionValue: '100%'
-          }}
-        )
-      ])
-    ]),
   ],
 })
 export class HeaderComponent implements OnInit {
 
+  // il faut mettre le state pour que l'anim fonctionne !
   slideLetterState: 'hidden' | 'show' = 'hidden'
 
   constructor() { }
