8693da0f34904cb19d7939fcf589ca877be44024
Jean Balangue <jeanbalangue@hotmail.fr>
2023-04-03 09:26:13 +0200

    maj texte nodejs

--- src/app/shared/animations/fade-in-color.animation.ts
+++ src/app/shared/animations/fade-in-color.animation.ts
@@ -4,7 +4,7 @@ export const FadeInColorAnimation = animation([
     style({
         opacity: 0,
         color: "{{startColor}}",
-        'font-size': "2.5rem",
+        'font-size': "3.5rem",
     }),
     animate('{{timer}} {{delay}} ease-out', style({
         opacity: 1,
