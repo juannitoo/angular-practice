cbe51be48469ea47ffac9c5e2bed877a7892d737
Jean Balangue <jeanbalangue@hotmail.fr>
2023-04-07 08:26:51 +0200

    maj anim logo

--- src/app/shared/animations/fade-in-color.animation.ts
+++ src/app/shared/animations/fade-in-color.animation.ts
@@ -4,11 +4,17 @@ export const FadeInColorAnimation = animation([
     style({
         opacity: 0,
         color: "{{startColor}}",
-        'font-size': "2.5rem",
+        'font-size': "2.3rem",
+        'position' : "relative",
+        'top': "-70px",
+        'left': '0px'
     }),
     animate('{{timer}} {{delay}} ease-out', style({
         opacity: 1,
         color: "{{endColor}}",
         'font-size': "1.6rem",
+        'position' : "relative",
+        'top': "0px",
+        'left': '0px'
     }))
 ])
