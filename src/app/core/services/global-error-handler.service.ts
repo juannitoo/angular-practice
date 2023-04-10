f55f177b40852d1821e8ccdd1c6e3638be4040c4
Jean Balangue <jeanbalangue@hotmail.fr>
2023-04-10 16:49:45 +0200

    global error exemple subject en erreur

--- src/app/core/services/global-error-handler.service.ts
+++ src/app/core/services/global-error-handler.service.ts
@@ -1,18 +1,24 @@
-import { Injectable, Injector, ErrorHandler } from '@angular/core'
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable, Injector, ErrorHandler, } from '@angular/core'
 import { Router } from '@angular/router'
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable()
 export class GlobalErrorHandlerService implements ErrorHandler {
+  
+  subject$ = new BehaviorSubject<string>("step1")
 
   // Angular creates the error handler service before the providers.
   // donc on utilise injector pour provide le routeur
   constructor( private injector: Injector) { }
+  
+  handleError(error : any) {
+    
+    this.subject$.next('step2')
 
-  handleError(error : Error) {
     let router = this.injector.get(Router);
     console.error(`Erreur Globale ici: ${router.url}
-    ${error.message}`)
+    ${error.message }`)
   }
 
 }
--- src/app/json-place-holder/components/users-list/users-list.component.ts
+++ src/app/json-place-holder/components/users-list/users-list.component.ts
@@ -1,15 +1,18 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Injector, OnDestroy, OnInit } from '@angular/core';
 import { User } from 'src/app/core/models/user.model';
 import { Observable, Subscription, of } from 'rxjs';
 import { UsersService } from 'src/app/core/services/users.services';
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { SlideAndFadeAnimation } from 'src/app/shared/animations/slide-and-fade.animation';
 import { ErrorsService } from 'src/app/core/services/errors.service';
+import { GlobalErrorHandlerService } from 'src/app/core/services/global-error-handler.service';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss'],
+  providers: [GlobalErrorHandlerService],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
     trigger( 'slideText', [
       transition('void => *', [
@@ -23,7 +26,6 @@ import { ErrorsService } from 'src/app/core/services/errors.service';
       })
     ])
   ])],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsersListComponent implements OnInit, OnDestroy { 
 
@@ -35,18 +37,27 @@ export class UsersListComponent implements OnInit, OnDestroy {
 
   constructor( private usersServ : UsersService,
                 private errorsService : ErrorsService,
-                private changeDetector: ChangeDetectorRef) { }
+                private changeDetector: ChangeDetectorRef,
+                private injector: Injector,
+                private globalErrorService : GlobalErrorHandlerService) { }
 
   ngOnInit(): void {
     this.users = this.usersServ.users$
     this.usersServ.getUsers()
-    this.errorSubscription = this.errorsService.httpErrors$.subscribe( val => {
-      if (val.message !== ""){
+    this.errorSubscription = this.errorsService.httpErrors$.subscribe( err => {
+      if (err.message !== ""){
         this.error = true
-        this.message = val.message
+        this.message = err.message
         this.changeDetector.detectChanges()
       }
     })
+
+    // let globalErrorService = this.injector.get(GlobalErrorHandlerService)
+    this.globalErrorService.subject$.subscribe((val)=>{
+      console.log('+++++++', val)  // n'affiche jamais step2
+      this.changeDetector.detectChanges()
+    })
+
   }
 
   ngOnDestroy(): void {
