57e96fe74da14949159994eefb602b83b50e408b
Jean Balangue <jeanbalangue@hotmail.fr>
2023-06-24 19:11:28 +0200

    Test landing-page component

---
+++ src/app/landing-page/landing-page.spec.ts
@@ -0,0 +1,32 @@
+import { waitForAsync, TestBed, ComponentFixture } from '@angular/core/testing'
+import { LandingPageComponent } from './landing-page.component'
+import { By } from '@angular/platform-browser'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+
+describe('landing-page Component', () => {
+  let component: LandingPageComponent
+  let fixture : ComponentFixture<LandingPageComponent>
+  let title: HTMLElement
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      declarations: [ LandingPageComponent ],
+      imports: [  NoopAnimationsModule ] ,
+    })
+    .createComponent(LandingPageComponent)
+  
+    fixture.detectChanges()
+
+    component = fixture.componentInstance
+    title = fixture.debugElement.query(By.css('h1')).nativeElement
+  })
+
+
+  it('should create component', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should return h1', () => {
+    expect(title.textContent).toBe("Mon initiation avec Angular")
+  })
+})
