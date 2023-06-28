52074592c3fa66d3a191359f6af2cc208dab9241
Jean Balangue <jeanbalangue@hotmail.fr>
2023-06-28 09:47:19 +0200

    test finalisation json-server users-list component

--- src/app/json-server/components/users-list/users-list.component.spec.ts
+++ src/app/json-server/components/users-list/users-list.component.spec.ts
@@ -9,6 +9,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core'
 import { Router, RouterLink, Routes } from '@angular/router'
 import { UserCreateComponent } from '../user-create/user-create.component'
 import { RouterTestingModule } from '@angular/router/testing'
+import { of } from 'rxjs'
 
 
 describe('json-server users-list Component', () => {
@@ -18,6 +19,56 @@ describe('json-server users-list Component', () => {
   let progressBar: DebugElement
   let appError: DebugElement
   let addUserLink: DebugElement
+  let jsUsersService : JsUsersService
+
+  let users = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz",
+      "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+          "lat": "-37.3159",
+          "lng": "81.1496"
+        }
+      },
+      "phone": "1-770-736-8031 x56442",
+      "website": "hildegard.org",
+      "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+      }
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv",
+      "address": {
+        "street": "Victor Plains",
+        "suite": "Suite 879",
+        "city": "Wisokyburgh",
+        "zipcode": "90566-7771",
+        "geo": {
+          "lat": "-43.9509",
+          "lng": "-34.4618"
+        }
+      },
+      "phone": "010-692-6593 x09125",
+      "website": "anastasia.net",
+      "company": {
+        "name": "Deckow-Crist",
+        "catchPhrase": "Proactive didactic contingency",
+        "bs": "synergize scalable supply-chains"
+      }
+    }
+  ]
 
   beforeEach( async() => {
     await TestBed.configureTestingModule({
@@ -59,14 +110,14 @@ describe('json-server users-list Component', () => {
     expect(progressBar.attributes).toEqual({})
   }))
 
-  it('should have link to add user page with routerlink directive', fakeAsync(() => {
+  it('should have link to addUser page with routerlink directive', fakeAsync(() => {
     component.isServerResponse = true
     component.errors.error = false
     fixture.detectChanges()
     addUserLink = fixture.debugElement.query(By.directive(RouterLink))
     expect(addUserLink).toBeTruthy()
     expect(addUserLink.nativeElement.href).toContain('/create')
-    // Expected 'http://localhost:9876/create' to be '*/create'. so contain
+    // Expected 'http://localhost:9876/create' toBe '/create' so toContain
   }))
 
   it('should show error message when needed', fakeAsync(() => {
@@ -75,6 +126,25 @@ describe('json-server users-list Component', () => {
     fixture.detectChanges()
     appError = fixture.debugElement.query(By.css('app-error'))
     expect(appError).toBeTruthy()
+  }))
+
+  it('should show users', fakeAsync(() => {
+    component.isServerResponse = true
+    component.errors.error = false
+    component.users$ = of(users) // sans ca, app-user vaut null et pas de users !
+    fixture.detectChanges()
+    expect(fixture.debugElement.query(By.css('app-user'))).not.toBeNull()
+
+    // c'est plutôt comme ca dans la doc officielle, mais ca ne fonctionne pas ici !
+    
+    // jsUsersService = TestBed.inject(JsUsersService)
+    // spyOn(jsUsersService, 'getUsers').and.returnValue(of(users))
+    // jsUsersService.getUsers().subscribe((res) => {
+    //   console.log(res) // affiche les users mais n'instancie pas le component via | async
+    //   fixture.detectChanges()
+    //   expect(fixture.debugElement.query(By.css('app-user'))).not.toBeNull()
+    // })
+
   }))
 
 })
