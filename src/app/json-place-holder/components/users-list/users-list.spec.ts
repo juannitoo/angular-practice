681c9324e3998857bcf26a3955d667445d5783e7
Jean Balangue <jeanbalangue@hotmail.fr>
2023-06-30 21:08:01 +0200

    test jsonPlaceHolder users-list component on push

--- src/app/json-place-holder/components/users-list/users-list.spec.ts
+++ src/app/json-place-holder/components/users-list/users-list.spec.ts
@@ -1,18 +1,17 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, DebugElement, NO_ERRORS_SCHEMA } from "@angular/core";
-import { async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, tick, waitForAsync } from "@angular/core/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, DebugElement } from "@angular/core";
+import { ComponentFixture, fakeAsync, TestBed, waitForAsync } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { defer, noop, Observable, of } from "rxjs";
-import { User } from "src/app/core/models/user.model";
+import { of } from "rxjs";
 import { UsersService } from "src/app/core/services/users.services";
 import { UsersListComponent } from "./users-list.component";
-import { HttpClientTestingModule } from '@angular/common/http/testing'
-import { UserComponent } from "../user/user.component";
 import { ErrorsService } from "src/app/core/services/errors.service";
+import { RouterLink } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
 
 // NoopAnimations pour enlever les anims
-// https://stackoverflow.com/questions/63839249/unit-testing-angular-component-with-input
 // https://stackoverflow.com/questions/55927441/how-can-i-test-a-modals-elements-with-ng-template-and-the-action-that-triggers
+// https://stackoverflow.com/questions/40895855/testing-onpush-components-in-angular-2
 
 describe('jsonPlaceHolder Users-list component', () => {
 
@@ -65,51 +64,65 @@ describe('jsonPlaceHolder Users-list component', () => {
     }
   ]
 
-
   let fixture: ComponentFixture<UsersListComponent>
-  let usersService: UsersService
   let container: DebugElement
   let article: DebugElement
-  let appUser: DebugElement
   let appError: DebugElement
-  let component: Component
+  let component: UsersListComponent
+  let mockUsersService: jasmine.SpyObj<UsersService>
+  let getUsersSpy : jasmine.Spy
+  let changeDetector: ChangeDetectorRef
   
   beforeEach( 
     waitForAsync(() => { 
+      mockUsersService = jasmine.createSpyObj<UsersService>('UsersService', ['getUsers', 'deleteUser'])
+      getUsersSpy = mockUsersService.getUsers.and.returnValue(of(users))
+      
       TestBed.configureTestingModule({
         declarations:[UsersListComponent],
-        imports: [  NoopAnimationsModule, HttpClientTestingModule ] ,
-        providers: [UsersService, NoopAnimationsModule, ErrorsService],
+        imports: [  NoopAnimationsModule, 
+                    RouterTestingModule,
+                    RouterLink ],
+        providers: [{ provide: UsersService, useValue: mockUsersService }, 
+                    ErrorsService,
+                    ChangeDetectorRef ],
         schemas : [CUSTOM_ELEMENTS_SCHEMA]
       }).compileComponents()
-      .then(() => {
-        fixture = TestBed.createComponent(UsersListComponent)
-        usersService = TestBed.inject(UsersService)       
-        spyOn(usersService, 'getUsers').and.returnValue(of(users))
-        usersService.getUsers()
-        fixture.detectChanges()
-        container = fixture.debugElement.query(By.css('.container'))
-        article = fixture.debugElement.query(By.css('article'))
-        appUser = fixture.debugElement.query(By.css('app-user'))
-        // container.childNodes.forEach((x)=>console.log("x: ",x.nativeNode))
-      })     
-    })
-    )
+    }) 
+  )
+
+  beforeEach( () => {
+    fixture = TestBed.createComponent(UsersListComponent)    
+    changeDetector = TestBed.inject(ChangeDetectorRef)      
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    container = fixture.debugElement.query(By.css('.container'))
+    article = fixture.debugElement.query(By.css('article'))
+  })     
+
     
   it("should load the template", () => {
     expect(container).not.toBeNull()   
   })
   
-  // je souhaite retrouver tous les <app-user> de mon template
-  // mais je n'y arrive pas. 
-  // <app-user *ngFor="let x of users | async" [user]="x"></app-user>
-  // pbme de ng-container très certainement
-  xit('should list users', () => {   
-    expect(article).toBeTruthy()
-    expect(appError).toBeTruthy()
-    // expect(appUser.properties['id']).toBe("1")
-    // expect(appUser.length).toBeGreaterThan(1)
+  it("should have link to add user", () => {
+    let link = fixture.debugElement.query(By.directive(RouterLink))
+    expect(link.nativeElement.href).toContain("create")
   })
+    
+  it('should list users', fakeAsync(() => {   
+    component.error = false
+    component.users = getUsersSpy()
+    // do a change detection on the real changeDetectionRef when use onPush strategy
+    fixture.componentRef.injector.get(ChangeDetectorRef).detectChanges()
+    expect(article.childNodes.length).toBeGreaterThan(1)
+  }))
 
+  it("should show <app-error> if an error occurs", () => {
+    component.error = true
+    fixture.componentRef.injector.get(ChangeDetectorRef).detectChanges()
+    appError = fixture.debugElement.query(By.css('app-error'))
+    expect(appError).toBeTruthy()
+  })
   
 })
