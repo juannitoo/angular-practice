cc3bb1941e230b028278c666ccc2f29f9f7f4fdf
Jean Balangue <jeanbalangue@hotmail.fr>
2023-06-30 15:20:34 +0200

    test jsonPlaceHolder module

--- src/app/json-place-holder/components/json-landing/json-landing.component.ts
+++ src/app/json-place-holder/components/json-landing/json-landing.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-json-landing',
   templateUrl: './json-landing.component.html',
   styleUrls: ['./json-landing.component.scss']
 })
-export class JsonLandingComponent implements OnInit {
+export class JsonLandingComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
---
+++ src/app/json-place-holder/json-place-holder.module.spec.ts
@@ -0,0 +1,44 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { JsonPlaceHolderModule } from "./json-place-holder.module"
+import { JsonLandingComponent } from "./components/json-landing/json-landing.component"
+import { UserComponent } from "./components/user/user.component"
+import { UserCreateComponent } from "./components/user-create/user-create.component"
+import { UserUpdateComponent } from "./components/user-update/user-update.component"
+import { UserDetailsComponent } from "../json-server/components/user-details/user-details.component"
+import { UsersListComponent } from "./components/users-list/users-list.component"
+import { RouterTestingModule } from "@angular/router/testing"
+import { ReactiveFormsModule } from "@angular/forms"
+import { SharedModule } from "../shared/shared.module"
+import { HttpClientTestingModule } from "@angular/common/http/testing"
+import { UsersService } from "../core/services/users.services"
+import { JsonPlaceHoldeRoutingModule } from "./json-place-holder-routing.module"
+
+
+describe('Json-Place-Holder Module', () => {
+  let fixture : ComponentFixture<JsonPlaceHolderModule>
+  let component: JsonLandingComponent
+
+  beforeEach( async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserComponent,
+                      UserCreateComponent,
+                      UserUpdateComponent,
+                      UserDetailsComponent,
+                      UsersListComponent,
+                      JsonLandingComponent
+                     ],
+      imports: [RouterTestingModule, 
+                ReactiveFormsModule, 
+                SharedModule,
+                HttpClientTestingModule
+              ],
+      providers : [ JsonPlaceHoldeRoutingModule, UsersService ]
+    })
+    fixture = TestBed.createComponent(UserCreateComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should start !', () => {
+    expect(component).toBeTruthy()
+  })
+})
