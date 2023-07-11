8227d576e239b33599e99c05515a64644948230f
Jean Balangue <jeanbalangue@hotmail.fr>
2023-07-11 10:59:57 +0200

    node sign up component validation

--- src/app/node/components/sign-up/sign-up.component.ts
+++ src/app/node/components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Data } from '@angular/router';
+import { Observable, map } from 'rxjs';
 import { NodeService } from 'src/app/core/services/node.service';
 import { confirmEqualValidator } from 'src/app/shared/validators/passwords.validators';
 
@@ -9,31 +10,35 @@ import { confirmEqualValidator } from 'src/app/shared/validators/passwords.valid
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit, DoCheck{
+export class SignUpComponent implements OnInit {    // DoCheck
 
   buttonValue! : string
   signUpForm! : FormGroup
   password!: FormControl
   confirmPassword!: FormControl
   passwordForm!: FormGroup
+  showPasswordsError$!: Observable<boolean>
 
   constructor( private formBuilder: FormBuilder,
               private nodeService: NodeService,
               ) {}
 
   ngOnInit(): void {
+
     this.signUpForm = this.formBuilder.group({
       email: [null, [ Validators.required, Validators.email ]],
       connectionPassword: [null, [Validators.required, Validators.minLength(5)]],
     }, {
       updateOn: 'change'
     })
+
     this.buttonValue = "Se connecter"
+
   }
 
-  ngDoCheck(){
-    // this.testFormValidity()
-  }
+  // ngDoCheck(){
+  //   this.testFormValidity()
+  // }
 
   onSubmitForm(){
     const data : Data = { 
@@ -51,7 +56,9 @@ export class SignUpComponent implements OnInit, DoCheck{
 
   chooseForm(formType: string){
     this.buttonValue = "Se connecter"
+
     if (formType === "inscription"){
+
       this.buttonValue = "S'inscrire"
       this.signUpForm.removeControl("connectionPassword")
       this.password = this.formBuilder.control(null, [Validators.required, Validators.minLength(5)] )
@@ -65,14 +72,22 @@ export class SignUpComponent implements OnInit, DoCheck{
         ...this.signUpForm.controls,
         password: this.passwordForm
       })
+      this.showPasswordsError$ = this.passwordForm.statusChanges.pipe(
+        map(status => status === 'INVALID' && this.password.value && this.confirmPassword.value )
+      )
+
     } else {
+
       this.signUpForm.addControl("connectionPassword", this.formBuilder.control('', [Validators.required, Validators.minLength(5)]))
+
     }
+
   }
 
   testFormValidity(){
     console.log("signup: ", this.signUpForm.status)
-    // console.log("paswword :", this.passwordForm.status)
+    console.log("paswword :", this.passwordForm.status)
+    console.log('---', this.confirmPassword.value) // génère erreur, obs pas init
   }
 
 
