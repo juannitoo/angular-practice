9855977cf51a814f216f6641fdead8bb6f4ab198
Jean Balangue <jeanbalangue@hotmail.fr>
2023-07-14 16:01:03 +0200

    addresse aws 1er test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001',  // adresse AWS conteneur serveur node à renseigner
+  apiUrl: 'http://13.38.84.1:3001',  // adresse AWS conteneur serveur node à renseigner
 };
